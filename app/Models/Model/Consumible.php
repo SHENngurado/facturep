3685b9567cf4ecb82649f5b4e41bf2f5f74f7457
Iñigo Segurado Garcia <segu_86@hotmail.com>
2022-10-31 12:49:14 +0100

    Initial commit

---
+++ app/Models/Model/Consumible.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Consumible extends Model
+{
+    use HasFactory;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'nombre','preciounidad','cantidad','importe','factura_id'
+    ];
+
+    public function factura(){
+                return $this->belongsTo('App\Models\Model\Factura','factura_id','id');
+                //
+    }
+}
